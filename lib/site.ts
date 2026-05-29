4ed27b2977b4185ce1c0a84d56af13fc9bb8d176
Alvin Al <alvvxal@gmail.com>
2026-05-29 11:51:40 +0700

    chore: change canonical domain from www to non-www

--- lib/site.ts
+++ lib/site.ts
@@ -1,4 +1,4 @@
-export const siteOrigin = "https://www.flaat.studio";
+export const siteOrigin = "https://flaat.studio";
 export const siteUrl = new URL(siteOrigin);
 export const siteName = "Flaat Studio";
 export const ogImagePath = "/assets/images/og-image.webp";
